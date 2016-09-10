4a1666c03f1bafe1fd68abf06e2a3fa235ef6827
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-09-11 01:42:04 +0200

    feat(app): Clock is ticking

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,18 +1,31 @@
 'use strict';
 
 import React from 'react';
+import moment from 'moment';
+import 'moment/locale/nb';
 
 require('styles/Clock.scss');
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    /* FIXME: Pass in initial server time/offset? */
+    this.state = {
+      time: '00:00'
+    }
+  }
   render() {
-    return <div className="clock">{this.props.time}</div>
+    return <div className="clock">{this.state.time}</div>
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        time: moment().format('HH:mm')
+      });
+    }, 1000);
   }
 }
 
 Clock.displayName = 'Clock';
-Clock.defaultProps = {
-  time: '00:00'
-};
 
 export default Clock;
