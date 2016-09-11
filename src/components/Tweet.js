e8f00f633cd04fcfe66281b61d22dc844fc825f9
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-09-11 14:28:13 +0200

    feat(app): relative tweet time

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,10 +1,32 @@
 'use strict';
 
 import React from 'react';
+import moment from 'moment';
+import 'moment/locale/nb';  // FIXME: load dynamically?
 
 require('styles/Tweet.scss');
 
 class Tweet extends React.Component {
+
+  static getRelativeTime(time, locale) {
+    const DATETIME_FORMAT = 'MMM DD HH:mm:ss Z YYYY';
+    moment.locale(locale); // format in locale
+
+    time = time.slice(4); // without weekday
+    var when = moment(time, DATETIME_FORMAT);
+
+    var now = moment().utc();
+    return when.from(now);
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      createdAtRelative: Tweet.getRelativeTime(props.status.created_at, props.locale)
+    }
+  }
+
   render() {
     return <div className="tweet">
         <div className="profile">
@@ -16,7 +38,7 @@ class Tweet extends React.Component {
           <span className="screen-name">
           <a href={'https://twitter.com/' + this.props.status.user.screen_name}>{this.props.status.user.screen_name}</a></span>
           {' ' + this.props.status.text}<br/>
-          <a href={'https://twitter.com/' + this.props.status.user.screen_name + '/status/' + this.props.status.id_str} className="when" title={this.props.status.created_at}>{this.props.status.created_at}</a>
+          <a href={'https://twitter.com/' + this.props.status.user.screen_name + '/status/' + this.props.status.id_str} className="when" title={this.props.status.created_at}>{this.state.createdAtRelative}</a>
         </div>
       </div>
   }
@@ -24,7 +46,9 @@ class Tweet extends React.Component {
 
 Tweet.displayName = 'Tweet';
 Tweet.defaultProps = {
-  status: {}
+  status: {},
+  locale: 'nb'
+
 };
 
 export default Tweet;
